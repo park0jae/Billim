e24267852a87b1358a005cabf22b7e15da3cfe2d
Dongwoongkim <rlaehddnd0422@naver.com>
2023-06-28 19:34:59 +0900

    initialize

---
+++ src/main/java/dblab/sharing_flatform/SharingFlatformApplication.java
@@ -0,0 +1,13 @@
+package dblab.sharing_flatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingFlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingFlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dblab/sharing_flatform/SharingFlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dblab.sharing_flatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingFlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
