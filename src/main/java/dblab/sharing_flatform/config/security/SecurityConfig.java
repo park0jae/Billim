79c374fa26013ccf88e1ea5a0cfe7f7d93d8e32b
Dongwoongkim <rlaehddnd0422@naver.com>
2023-07-04 15:12:36 +0900

    securityConfig -category

--- src/main/java/dblab/sharing_flatform/config/security/SecurityConfig.java
+++ src/main/java/dblab/sharing_flatform/config/security/SecurityConfig.java
@@ -59,7 +59,13 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.GET, "/member/user").permitAll()
                 .antMatchers(HttpMethod.GET, "/member/admin/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/member/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.PATCH, "/member/**").hasAuthority("USER");
+                .antMatchers(HttpMethod.PATCH, "/member/**").hasAuthority("USER")
+
+
+                // 카테고리 생성 삭제 - ADMIN
+                .antMatchers(HttpMethod.DELETE, "/category/**").hasAuthority("ADMIN") // 카테고리 삭제, 생성은 ADMIN만 가능
+                .antMatchers(HttpMethod.POST, "/category/**").hasAuthority("ADMIN");
+
 
                 // 시큐리티 설정 끝
 
