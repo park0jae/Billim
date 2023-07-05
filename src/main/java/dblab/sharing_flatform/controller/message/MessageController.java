b96a1118e681834bd98c28ae95a4cc9e8cbfc1f6
park0jae <okvv26@naver.com>
2023-07-05 19:45:05 +0900

    MessageRequestDto & MessageController Swagger 설정

--- src/main/java/dblab/sharing_flatform/controller/message/MessageController.java
+++ src/main/java/dblab/sharing_flatform/controller/message/MessageController.java
@@ -6,6 +6,8 @@ import dblab.sharing_flatform.dto.response.Response;
 import dblab.sharing_flatform.exception.auth.AccessDeniedException;
 import dblab.sharing_flatform.service.message.MessageService;
 import dblab.sharing_flatform.util.SecurityUtil;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,7 @@ public class MessageController {
 
     private final MessageService messageService;
 
+    @ApiOperation(value = "메세지 생성 및 전송", notes = "메세지를 생성하고 수신자에게 전송합니다.")
     @PostMapping
     public Response sendMessageToReceiver(@RequestBody MessageRequestDto messageRequestDto) {
         String username = SecurityUtil.getCurrentUsername().orElseThrow(AccessDeniedException::new);
@@ -27,6 +30,7 @@ public class MessageController {
         return Response.success(messageResponseDto);
     }
 
+    @ApiOperation(value = "송신 메시지 조회", notes = "현재 로그인한 유저가 송신한 메시지를 조회합니다.")
     @GetMapping("/send")
     public Response findSendMessage(){
         String senderName = SecurityUtil.getCurrentUsername().orElseThrow(AccessDeniedException::new);
@@ -35,6 +39,7 @@ public class MessageController {
         return Response.success(sendMessage);
     }
 
+    @ApiOperation(value = "수신 메시지 조회", notes = "현재 로그인한 유저가 수신한 메시지를 조회합니다.")
     @GetMapping("/receive")
     public Response findReceiveMessage(){
         String receiverName = SecurityUtil.getCurrentUsername().orElseThrow(AccessDeniedException::new);
@@ -43,31 +48,35 @@ public class MessageController {
         return Response.success(receiveMessage);
     }
 
+    @ApiOperation(value = "특정 송신 메시지 조회", notes = "현재 로그인한 유저가 특정 유저에게 송신한 메세지를 조회합니다.")
     @GetMapping("/send/{id}")
-    public Response findSendMessageToMember(@PathVariable Long id){
+    public Response findSendMessageToMember(@ApiParam(value = "송신자 id", required = true) @PathVariable Long id){
         String senderName = SecurityUtil.getCurrentUsername().orElseThrow(AccessDeniedException::new);
         List<MessageResponseDto> sendMessageToMember = messageService.findSendMessageToMember(senderName, id);
 
         return Response.success(sendMessageToMember);
     }
 
+    @ApiOperation(value = "특정 수신 메시지 조회", notes = "현재 로그인한 유저가 특정 유저로부터 수신한 메세지를 조회합니다.")
     @GetMapping("/receive/{id}")
-    public Response findReceiveMessageByMember(@PathVariable Long id){
+    public Response findReceiveMessageByMember(@ApiParam(value = "수신자 id", required = true) @PathVariable Long id){
         String receiverName = SecurityUtil.getCurrentUsername().orElseThrow(AccessDeniedException::new);
         List<MessageResponseDto> sendMessageToMember = messageService.findReceiveMessageByMember(receiverName, id);
 
         return Response.success(sendMessageToMember);
     }
 
+    @ApiOperation(value = "송신자에 의한 메세지 삭제", notes = "메세지를 보낸 유저가 메세지를 삭제합니다.")
     @DeleteMapping("/send/{id}")
-    public Response deleteBySender(@PathVariable Long id) {
+    public Response deleteBySender(@ApiParam(value = "송신자 id", required = true) @PathVariable Long id) {
         // 보낸 메세지가 자신의 소유인지 확인 - messageGuard
         messageService.deleteMessageBySender(id);
         return Response.success();
     }
 
+    @ApiOperation(value = "수신자에 의한 메세지 삭제", notes = "메세지를 수신한 유저가 메세지를 삭제합니다.")
     @DeleteMapping("/receive/{id}")
-    public Response deleteByReceiver(@PathVariable Long id) {
+    public Response deleteByReceiver(@ApiParam(value = "수신자 id", required = true) @PathVariable Long id) {
         messageService.deleteMessageByReceiver(id);
         return Response.success();
     }
--- src/main/java/dblab/sharing_flatform/dto/message/MessageRequestDto.java
+++ src/main/java/dblab/sharing_flatform/dto/message/MessageRequestDto.java
@@ -3,6 +3,7 @@ package dblab.sharing_flatform.dto.message;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import dblab.sharing_flatform.domain.member.Member;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,16 +11,20 @@ import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.Null;
 
+@ApiModel(value = "메세지 생성 요청")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MessageRequestDto {
 
+    @ApiModelProperty(value = "메세지 내용", notes = "메세지 내용을 입력해주세요.", required = true)
     private String content;
 
+    @ApiModelProperty(value = "송신자 이름", notes = "송신자 이름을 입력해주세요", required = true)
     @Nullable
     private String sendMember;
 
+    @ApiModelProperty(value = "수신자 이름", notes = "수신자 이름을 입력해주세요", required = true)
     private String receiveMember;
 
 }
