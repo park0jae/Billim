1469c71ed8173fa9136591eba72c71b2e480c4c1
park0jae <okvv26@naver.com>
2023-07-25 12:53:06 +0900

    refactoring

--- src/main/java/dblab/sharing_flatform/config/listener/NotificationListener.java
+++ src/main/java/dblab/sharing_flatform/config/listener/NotificationListener.java
@@ -1,6 +1,8 @@
 package dblab.sharing_flatform.config.listener;
 
 import dblab.sharing_flatform.dto.notification.crud.create.NotificationRequestDto;
+import dblab.sharing_flatform.repository.emitter.EmitterRepository;
+import dblab.sharing_flatform.repository.member.MemberRepository;
 import dblab.sharing_flatform.service.notification.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
@@ -12,7 +14,6 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class NotificationListener {
 
     private final NotificationService notificationService;
-
     @TransactionalEventListener
     @Async
     public void handleNotification(NotificationRequestDto requestDto){
