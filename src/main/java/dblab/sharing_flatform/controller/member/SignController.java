b72a85cd48536c006be7cf41b73cdd178ebd15d3
Dongwoongkim <rlaehddnd0422@naver.com>
2023-07-26 20:56:58 +0900

    oauth2 중복 회원가입 추가

--- src/main/java/dblab/sharing_flatform/controller/member/SignController.java
+++ src/main/java/dblab/sharing_flatform/controller/member/SignController.java
@@ -5,6 +5,13 @@ import dblab.sharing_flatform.dto.member.login.LoginRequestDto;
 import dblab.sharing_flatform.dto.member.crud.create.MemberCreateRequestDto;
 import dblab.sharing_flatform.dto.oauth.crud.create.AccessTokenRequestDto;
 import dblab.sharing_flatform.dto.response.Response;
+<<<<<<< HEAD
+=======
+import dblab.sharing_flatform.exception.member.MemberNotFoundException;
+import dblab.sharing_flatform.exception.oauth.OAuthUserNotFoundException;
+import dblab.sharing_flatform.exception.oauth.SocialAgreementException;
+import dblab.sharing_flatform.service.mail.NaverMailService;
+>>>>>>> ed14658ce3d76f7f866d9f9fb87ef69b52c40ea9
 import dblab.sharing_flatform.service.member.MemberService;
 import dblab.sharing_flatform.service.member.SignService;
 import dblab.sharing_flatform.service.oauth.OAuthService;
@@ -17,7 +24,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
 
 
 @Api(value = "Sign Controller", tags = "Sign")
@@ -27,6 +36,11 @@ import javax.validation.Valid;
 public class SignController {
     private final SignService signService;
     private final OAuthService oAuthService;
+<<<<<<< HEAD
+=======
+    private final MemberService memberService;
+    private final NaverMailService naverMailService;
+>>>>>>> ed14658ce3d76f7f866d9f9fb87ef69b52c40ea9
 
     @ApiOperation(value = "일반 회원가입", notes = "일반 회원가입을 한다.") // 2
     @PostMapping("/sign-up")
@@ -50,6 +64,14 @@ public class SignController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @ApiOperation(value = "이메일 인증" , notes = "회원가입 시 이메일 인증을 위한 엔드포인트")
+    @ResponseBody
+    @PostMapping("/mail")
+    public Response mailConfirm(@RequestParam(name = "email") String email)  {
+        String code = naverMailService.sendSimpleMessage(email);
+        return Response.success(code);
     }
 
     @ApiOperation(value = "카카오 로그인", notes = "OAuth2.0 카카오로 소셜 로그인을 진행합니다.")
