be60af88102ab8fa5890b705c6bef6f0e2687dab
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-09 15:07:03 +0900

    CategoryFactory 개행 수정

--- src/test/java/dblab/sharing_platform/factory/category/CategoryFactory.java
+++ src/test/java/dblab/sharing_platform/factory/category/CategoryFactory.java
@@ -3,16 +3,13 @@ package dblab.sharing_platform.factory.category;
 import dblab.sharing_platform.domain.category.Category;
 
 public class CategoryFactory {
-    public static Category  createCategory() {
-        return new Category("category1", null);
+    public static Category createCategory() {
+        return new Category("category", null);
     }
     public static Category createCategory(String name, Category parent) {
         return new Category(name, parent);
     }
-
     public static Category createCategoryWithName(String name) {
         return new Category(name, null);
     }
-
-
 }
